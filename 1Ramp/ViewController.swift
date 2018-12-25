7df67c3044d8b700f73f4c12444c25f8418b8ba5
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-12-25 14:45:23 +0530

    Initial Commit

---
+++ 1Ramp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  1Ramp
+//
+//  Created by Ankit Kumar on 25/12/18.
+//  Copyright © 2018 Hapramp Studios Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
